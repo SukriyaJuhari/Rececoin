d1e2abb6bd4c49dc4089fade5488bbb5ca70d281
sukriyajuhari <sukriyajuhari@gmail.com>
2025-12-04 21:33:45 +0700

    update

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 // --------------------------
 // THEME TOGGLE
 // --------------------------
@@ -11,7 +10,7 @@ if (localStorage.getItem("theme") === "dark") {
 
 themeBtn.addEventListener("click", () => {
   document.body.classList.toggle("dark");
-
+  
   if (document.body.classList.contains("dark")) {
     themeBtn.textContent = "☀️";
     localStorage.setItem("theme", "dark");
@@ -25,53 +24,94 @@ themeBtn.addEventListener("click", () => {
 
 
 //from registrasi function open/ 
-    function showRegister() {
-      document.getElementById('loginForm').classList.add('hidden');
-      document.getElementById('registerForm').classList.remove('hidden');
-    }
-    
-    function showLogin() {
-      document.getElementById('registerForm').classList.add('hidden');
-      document.getElementById('loginForm').classList.remove('hidden');
-    }
+function showRegister() {
+  document.getElementById('loginForm').classList.add('hidden');
+  document.getElementById('registerForm').classList.remove('hidden');
+}
+
+function showLogin() {
+  document.getElementById('registerForm').classList.add('hidden');
+  document.getElementById('loginForm').classList.remove('hidden');
+}
 //from registrasi function close/ 
 //##########################################################################
+
+
+let injectHtml = `
+  <div class="autoScroll"> 
+      <div class="item"><img src="assets/img/IMG_20251203_151105.jpg" alt="item"><b>Clash of Clans <br> $10</b></div>
+      <div class="item"><img src="assets/img/IMG_20251203_151147.jpg" alt="item"><b>Clash of Clans <br> $10</b></div>
+      <div class="item"><img src="assets/img/IMG_20251203_151235.jpg" alt="item"><b>Clash of Clans <br> $10</b></div>
+      <div class="item"><img src="assets/img/IMG_20251203_151326.jpg" alt="item"><b>Clash of Clans <br> $10</b></div>
+      <div class="item"><img src="assets/img/IMG_20251203_151422.jpg" alt="item"><b>Clash of Clans <br> $10</b></div>
+      <div class="item"><img src="assets/img/IMG_20251203_151537.jpg" alt="item"><b>Clash of Clans <br> $10</b></div>
+      <div class="item"><img src="assets/img/IMG_20251203_151622.jpg" alt="item"><b>Clash of Clans <br> $10</b></div>
+      <div class="item"><img src="assets/img/IMG_20251203_151729.jpg" alt="item"><b>Clash of Clans <br> $10</b></div>
+      <div class="item"><img src="assets/img/IMG_20251203_151826.jpg" alt="item"><b>Clash of Clans <br> $10</b></div>
+      <div class="item"><img src="assets/img/IMG_20251203_151910.jpg" alt="item"><b>Clash of Clans <br> $10</b></div>
+      <div class="item"><img src="assets/img/IMG_20251203_151947.jpg" alt="item"><b>Clash of Clans <br> $10</b></div>
+      <div class="item"><img src="assets/img/IMG_20251203_152039.jpg" alt="item"><b>Clash of Clans <br> $10</b></div>
+  </div>
+`;
+
+let deskAutoScrol = document.getElementById("deskAutoScroll");
+let mobileAutoScrol = document.getElementById("mobileAutoScroll");
+
 //conten autoScroll open
-const box = document.getElementById("autoScroll");
-let arah = 1; // 1 = kanan, -1 = kiri
-
-setInterval(() => {
-  const max = box.scrollWidth - box.clientWidth;
-  if (box.scrollLeft >= max-1) {
-    arah = -1; // balik ke kiri
-  } else if (box.scrollLeft <= 0) {
-    arah = 1; // balik ke kanan
-  }
-
-  box.scrollLeft += 2 * arah; // gerak sesuai arah
-}, 1);
+function startAutoScroll(box) {
+  let arah = 1;
+  setInterval(() => {
+    const max = box.scrollWidth - box.clientWidth;
+    if (box.scrollLeft >= max - 1) arah = -1;
+    else if (box.scrollLeft <= 0) arah = 1;
+    box.scrollLeft += 2 * arah;
+    
+  }, 1);
+}
 
 //conten autoScroll close
-//##########################################################################
 
 
 function checkMode() {
   const w = window.innerWidth;
   const h = window.innerHeight;
+  
+  if (h > w && h >= 600) {
+    deskAutoScrol.style.display = "none";
 
-  if (h > w && h < 600) {
-    console.log("Small Portrait");
-  } 
-  else if (h > w && h >= 600) {
+    mobileAutoScrol.innerHTML = injectHtml;
+    mobileAutoScrol.style.display = "block";
+
+    // jalankan auto-scroll pada elemen baru
+    const box = mobileAutoScrol.querySelector(".autoScroll");
+    if (box) startAutoScroll(box);
+    
     console.log("Large Portrait");
   }
   else if (w > h && h < 450) {
     console.log("Landscape Mobile");
+    mobileAutoScrol.style.display = "none";
+
+    deskAutoScrol.innerHTML = injectHtml;
+    deskAutoScrol.style.display = "block";
+
+    const box = deskAutoScrol.querySelector(".autoScroll");
+    if (box) startAutoScroll(box);
   }
   else {
     console.log("Desktop / Large Screen");
+    mobileAutoScrol.style.display = "none";
+
+    deskAutoScrol.innerHTML = injectHtml;
+    deskAutoScrol.style.display = "block";
+
+    const box = deskAutoScrol.querySelector(".autoScroll");
+    if (box) startAutoScroll(box);
   }
 }
 
 checkMode();
 window.addEventListener("resize", checkMode);
+
+
+//##########################################################################
