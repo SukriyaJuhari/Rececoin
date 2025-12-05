b8469569ea711c4f3f6c9ddcd2742dcc7190ff34
sukriyajuhari <sukriyajuhari@gmail.com>
2025-12-05 16:52:23 +0700

    update2512051653

--- main.js
+++ main.js
@@ -21,37 +21,46 @@ themeBtn.addEventListener("click", () => {
 });
 
 
-
-
 //from registrasi function open/ 
-function showRegister() {
-  document.getElementById('loginForm').classList.add('hidden');
-  document.getElementById('registerForm').classList.remove('hidden');
-}
+const loginForm = document.getElementById('loginForm');
+const registerForm = document.getElementById('registerForm');
 
-function showLogin() {
-  document.getElementById('registerForm').classList.add('hidden');
-  document.getElementById('loginForm').classList.remove('hidden');
-}
+document.getElementById("btnShowRegister").addEventListener("click", () => {
+  loginForm.classList.add("hidden");
+  registerForm.classList.remove("hidden");
+});
+
+document.getElementById("btnShowLogin").addEventListener("click", () => {
+  registerForm.classList.add("hidden");
+  loginForm.classList.remove("hidden");
+});
+
 //from registrasi function close/ 
+
+
 //##########################################################################
 
 
 let injectHtml = `
-  <div class="autoScroll"> 
-      <div class="item"><img src="assets/img/IMG_20251203_151105.jpg" alt="item"><b>Clash of Clans <br> $10</b></div>
-      <div class="item"><img src="assets/img/IMG_20251203_151147.jpg" alt="item"><b>Clash of Clans <br> $10</b></div>
-      <div class="item"><img src="assets/img/IMG_20251203_151235.jpg" alt="item"><b>Clash of Clans <br> $10</b></div>
-      <div class="item"><img src="assets/img/IMG_20251203_151326.jpg" alt="item"><b>Clash of Clans <br> $10</b></div>
-      <div class="item"><img src="assets/img/IMG_20251203_151422.jpg" alt="item"><b>Clash of Clans <br> $10</b></div>
-      <div class="item"><img src="assets/img/IMG_20251203_151537.jpg" alt="item"><b>Clash of Clans <br> $10</b></div>
-      <div class="item"><img src="assets/img/IMG_20251203_151622.jpg" alt="item"><b>Clash of Clans <br> $10</b></div>
-      <div class="item"><img src="assets/img/IMG_20251203_151729.jpg" alt="item"><b>Clash of Clans <br> $10</b></div>
-      <div class="item"><img src="assets/img/IMG_20251203_151826.jpg" alt="item"><b>Clash of Clans <br> $10</b></div>
-      <div class="item"><img src="assets/img/IMG_20251203_151910.jpg" alt="item"><b>Clash of Clans <br> $10</b></div>
-      <div class="item"><img src="assets/img/IMG_20251203_151947.jpg" alt="item"><b>Clash of Clans <br> $10</b></div>
-      <div class="item"><img src="assets/img/IMG_20251203_152039.jpg" alt="item"><b>Clash of Clans <br> $10</b></div>
+<h3>GoPlay-GoEarn!<h3/>
+<i>Earn money just by playing games!</i>
+	<div class="boxItem">
+  <div class="autoScroll">
+      <div class="item"><img src="assets/img/IMG_20251203_151147.jpg" alt="item"><b>Coin Master <br> $18</b></div>
+      <div class="item"><img src="assets/img/IMG_20251203_151235.jpg" alt="item"><b>Candy Crush <br> $15</b></div>
+      <div class="item"><img src="assets/img/IMG_20251203_151326.jpg" alt="item"><b>Angry Birds <br> $10</b></div>
+      <div class="item"><img src="assets/img/IMG_20251203_151105.jpg" alt="item"><b>Minecraft <br> $58</b></div>
+      <div class="item"><img src="assets/img/IMG_20251203_151422.jpg" alt="item"><b>Clash Royale <br> $24</b></div>
+      <div class="item"><img src="assets/img/IMG_20251203_151537.jpg" alt="item"><b>Clash of Clans <br> $28</b></div>
+      <div class="item"><img src="assets/img/IMG_20251203_151622.jpg" alt="item"><b>Rise of Kingdoms <br> $14</b></div>
+      <div class="item"><img src="assets/img/IMG_20251203_151729.jpg" alt="item"><b>Roblox <br> $48</b></div>
+      <div class="item"><img src="assets/img/IMG_20251203_151826.jpg" alt="item"><b>Fishdom <br> $11</b></div>
+      <div class="item"><img src="assets/img/IMG_20251203_151910.jpg" alt="item"><b>Pokémon GO <br> $16</b></div>
+      <div class="item"><img src="assets/img/IMG_20251203_151947.jpg" alt="item"><b>Geometry Dash <br> $21</b></div>
+      <div class="item"><img src="assets/img/IMG_20251203_152039.jpg" alt="item"><b>Monopoli <br> $14</b></div>
   </div>
+  </div>
+  	</div>
 `;
 
 let deskAutoScrol = document.getElementById("deskAutoScroll");
@@ -62,8 +71,8 @@ function startAutoScroll(box) {
   let arah = 1;
   setInterval(() => {
     const max = box.scrollWidth - box.clientWidth;
-    if (box.scrollLeft >= max - 1) arah = -1;
-    else if (box.scrollLeft <= 0) arah = 1;
+    if (box.scrollLeft >= max - 5) arah = -1; //ke kiri 
+    else if (box.scrollLeft <= 0) arah = 1; // ke kanan
     box.scrollLeft += 2 * arah;
     
   }, 1);
@@ -78,10 +87,10 @@ function checkMode() {
   
   if (h > w && h >= 600) {
     deskAutoScrol.style.display = "none";
-
+    
     mobileAutoScrol.innerHTML = injectHtml;
     mobileAutoScrol.style.display = "block";
-
+    
     // jalankan auto-scroll pada elemen baru
     const box = mobileAutoScrol.querySelector(".autoScroll");
     if (box) startAutoScroll(box);
@@ -91,20 +100,20 @@ function checkMode() {
   else if (w > h && h < 450) {
     console.log("Landscape Mobile");
     mobileAutoScrol.style.display = "none";
-
+    
     deskAutoScrol.innerHTML = injectHtml;
     deskAutoScrol.style.display = "block";
-
+    
     const box = deskAutoScrol.querySelector(".autoScroll");
     if (box) startAutoScroll(box);
   }
   else {
     console.log("Desktop / Large Screen");
     mobileAutoScrol.style.display = "none";
-
+    
     deskAutoScrol.innerHTML = injectHtml;
     deskAutoScrol.style.display = "block";
-
+    
     const box = deskAutoScrol.querySelector(".autoScroll");
     if (box) startAutoScroll(box);
   }
